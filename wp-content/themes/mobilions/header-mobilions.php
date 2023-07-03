9cc6e6742f385814957a1e904de4690effceb1af
unknown <krupesh.adorncommerce@gmail.com>
2023-07-04 02:16:36 +0530

    Header and footer design

--- wp-content/themes/mobilions/header-mobilions.php
+++ wp-content/themes/mobilions/header-mobilions.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex, nofollow">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <title>Mobilions</title>
     <link rel="icon" href="https://mobilions.nl/blogs/wp-content/themes/mobilions/assets/images/icons/favicon.svg">
